948a0dbcf1012ed97b191d8f2b5fdeab00db86ab
raghav1010 <raghavkol1997@gmail.com>
2023-10-24 08:10:04 -0700

    configured redis, celery and flower.

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -23,7 +23,7 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = ["rest_framework", "django_filters", "django_countries", "phonenumber_field", "drf_yasg",
-                    "corsheaders"]
+                    "corsheaders", "djcelery_email"]
 
 LOCAL_APPS = ["core_apps.profiles", "core_apps.common", "core_apps.users"]
 
@@ -137,6 +137,18 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_URLS_REGEX = r"^api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
+
 
 LOGGING = {
     "version": 1,
